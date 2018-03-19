eda46f30de3f6ef4b588c3f963201772d7937314
Emanuele Fumagalli <emanuele@macbook-pro-di-emanuele.local>
2018-03-19 23:57:15 +0000

    Managed to upload the generated pic

---
+++ VocabularyImprover/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  VocabularyImprover
+//
+//  Created by Emanuele Fumagalli on 18/03/18.
+//  Copyright © 2018 emafuma. All rights reserved.
+//
+
+import Foundation
+
+import AWSCore
+
+let S3BucketName: String = "vocabulary-images"
+let S3DownloadKeyName: String = "test-image.png"
+let S3UploadKeyName: String = "test-image.png"
--- VocabularyImprover/NewPictureController.swift
+++ VocabularyImprover/NewPictureController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import AWSS3
+import AWSCognito
 
 class NewPictureController: UIViewController,
-UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+UINavigationControllerDelegate {
     
     var imagePickerController : UIImagePickerController!
-
+    
+    var completionHandler: AWSS3TransferUtilityUploadCompletionHandlerBlock?
+    var progressBlock: AWSS3TransferUtilityProgressBlock?
+    lazy var transferUtility = AWSS3TransferUtility.default()
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func onTakePicture(_ sender: Any) {
@@ -23,40 +29,118 @@ UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     }
     
     @IBAction func onSendPicture(_ sender: Any) {
-        guard imageView.image != nil else {
+        guard let selectedImage = imageView.image else {
             print("No picture to send yet")
+            
+            self.uploadImage(with: UIImagePNGRepresentation(createImage(from: imageView))!)
             return
         }
+        
+        self.uploadImage(with: UIImagePNGRepresentation(selectedImage)!)
+    }
+    
+    func createImage(from view: UIView) -> UIImage {
+        UIGraphicsBeginImageContext(view.bounds.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let textColor = UIColor.white
+        let textFont = UIFont(name: "Helvetica Bold", size: 32)!
+        
+        let textFontAttributes = [
+            NSAttributedStringKey.font: textFont,
+            NSAttributedStringKey.foregroundColor: textColor,
+            ]
+        
+        let rect = view.bounds
+        
+        let drawText = "First try!"
+        
+        drawText.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let viewImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        imageView.image = viewImage
+        
+        return viewImage
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.imageView.backgroundColor = .blue
+        
+        self.progressBlock = {(task, progress) in
+            DispatchQueue.main.async(execute: {
+                print("Progress: \(Float(progress.fractionCompleted))")
+            })
+        }
+        
+        self.completionHandler = { (task, error) -> Void in
+            DispatchQueue.main.async(execute: {
+                if let error = error {
+                    print("Failed with error: \(error)")
+                }
+                else {
+                    print("Success uploading picture")
+                }
+            })
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func uploadImage(with data: Data) {
+        let expression = AWSS3TransferUtilityUploadExpression()
+        expression.progressBlock = progressBlock
+        
+        transferUtility.uploadData(
+            data,
+            bucket: S3BucketName,
+            key: S3UploadKeyName,
+            contentType: "image/png",
+            expression: expression,
+            completionHandler: completionHandler).continueWith { (task) -> AnyObject! in
+                if let error = task.error {
+                    print("Error: \(error.localizedDescription)")
+                    
+                    DispatchQueue.main.async {
+                        print("Failed")
+                    }
+                }
+                
+                if let _ = task.result {
+                    DispatchQueue.main.async {
+                        print("Upload Starting!")
+                    }
+                    // Do something with uploadTask.
+                }
+                
+                return nil;
+        }
+    }
+    
+    
+}
 
-    //MARK: UIImagePickerControllerDelegate
+//MARK: UIImagePickerControllerDelegate
+extension NewPictureController: UIImagePickerControllerDelegate {
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // Dismiss the picker if the user canceled.
         dismiss(animated: true, completion: nil)
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         
         guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
         
         imageView.image = selectedImage
-        
         dismiss(animated: true, completion: nil)
     }
-
 }
 
